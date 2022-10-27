4287e8f5821a0be84eee704b0495d8ab7fa0d644
raissa <raissa.campos26@gmail.com>
2022-10-27 10:48:51 -0300

    styles: comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="http://github.com/proudynyu.png"
+        alt=""
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Igor Becker</strong>
+              <time title="27 de outubro de 2022" dateTime="2022-10-27">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
